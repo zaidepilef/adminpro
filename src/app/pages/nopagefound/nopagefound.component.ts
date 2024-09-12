4c8cace2ebcb830c89dd4aab304af03eb8b89905
Felipe Diaz <fel.di.rod@gmail.com>
2024-09-12 20:21:48 -0300

    404 year onInit

--- src/app/pages/nopagefound/nopagefound.component.ts
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
   styleUrl: './nopagefound.component.css'
 })
-export class NopagefoundComponent {
+export class NopagefoundComponent implements OnInit {
+
+  year = new Date().getFullYear();
+  constructor() {
+
+  }
+
+  ngOnInit(): void { }
 
 }
