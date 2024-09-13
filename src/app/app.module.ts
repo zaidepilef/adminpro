f56eaf1885eba875ad53b1ae36e3f7cbd0cdca9f
Felipe Diaz <fel.di.rod@gmail.com>
2024-09-12 21:12:36 -0300

    todo funciona bien con modulos personalizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,38 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { Graficas2Component } from './pages/graficas2/graficas2.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    FooterComponent,
-    ProgressComponent,
-    RegisterComponent,
-    Graficas1Component,
-    NopagefoundComponent,
-    Graficas2Component,
-    PagesComponent
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [
     provideClientHydration()
