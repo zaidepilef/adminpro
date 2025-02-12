9cce6db25f3579448afe025af7dee42c54e2b221
Felipe Diaz <fel.di.rod@gmail.com>
2025-02-12 05:27:56 -0300

    se agregar auth controler

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,37 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm: FormGroup;
+	errorMessage: string | null = null;
+
+	constructor(
+		private fb: FormBuilder,
+		private authService: AuthService,
+		private router: Router,
+	) {
+		this.loginForm = this.fb.group({
+			email: ['', [Validators.required, Validators.email]],
+			password: ['', Validators.required],
+		});
+	}
+
+
+  onSubmit() {
+
+		if (this.loginForm.invalid) return;
+
+		const credentials = this.loginForm.value;
+
+		this.authService.login(credentials).subscribe({
+			next: (response) => {
+				
+				this.authService.saveToken(response);
+				this.router.navigate(['/home']); // Redirigir al Dashboard
+			},
+			error: (err) => {
+				this.errorMessage = 'Correo o contraseña incorrectos';
+				console.error('Error en login', err);
+			}
+		});
+	}
 }
---
+++ src/environment.prod.ts
@@ -0,0 +1,5 @@
+
+  export const environment = {
+    production: true,
+    apiUrl: 'https://api.tudominio.com/api'  // URL de la API en producción
+  };
